3a73ace2d673d9f51d0e8d31664d76e4552caf59
tarasMatskovich <matskovich.taras@gmail.com>
2019-02-07 19:12:22 +0200

    Add code

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
-echo 'Hello world';
+$fileName = 'data/INPUT.txt';
+$fn = fopen($fileName, 'r');
+
+$data = fgets($fn);
+
+$len = strlen($data);
+$counter = 0;
+$max = 0;
+for ($i = 0; $i < $len; $i++) {
+	if ($data[$i] == '1') {
+		$counter++;
+	} else {
+		if ($counter > $max) {
+			$max = $counter;
+		}
+		$counter = 0;
+	}
+}
+
+$max = ($counter > $max) ? $counter : $max;
+
+$outputFileName = 'data/OUTPUT.txt';
+file_put_contents($outputFileName, $max);
